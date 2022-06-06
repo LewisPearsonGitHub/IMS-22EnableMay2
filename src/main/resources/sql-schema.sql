a8a6e65cfe91bb96be61e38e3dacb7b8b0ba0063
LewisPearsonGitHub <lewiscpearson@gmail.com>
2022-06-06 15:42:55 +0100

    LPP-2 #in-progress #comment Added orders and items to database schema

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -5,8 +5,25 @@ CREATE SCHEMA IF NOT EXISTS `ims`;
 USE `ims` ;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`customer_id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `fk_customer_id` INT(11) DEFAULT NULL,
+    `fk_item_id` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`orders_id`),
+    FOREIGN KEY (`fk_customers_id`) REFERENCES `customers`(`customer_id`),
+    FOREIGN KEY (`fk_item_id`) REFERENCES `items`(`items_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `value` decimal(10,2) DEFAULT NULL,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
+

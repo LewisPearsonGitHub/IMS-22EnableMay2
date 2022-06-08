f8869307a1754e67cd4524e8999154139d5b90e3
LewisPearsonGitHub <lewiscpearson@gmail.com>
2022-06-08 16:39:21 +0100

    Changes to costs.java

--- src/main/java/com/qa/ims/persistence/domain/Costs.java
+++ src/main/java/com/qa/ims/persistence/domain/Costs.java
@@ -1,5 +1,27 @@
 package com.qa.ims.persistence.domain;
 
 public class Costs {
-
+	private Long orderId;
+	private Long cost;
+	
+	
+	public Long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+	public Long getCost() {
+		return cost;
+	}
+	public void setCost(Long cost) {
+		this.cost = cost;
+	}
+	@Override
+	public String toString() {
+		return "Costs [orderId=" + orderId + ", cost=" + cost + "]";
+	}
+	
+	
+	
 }

f1c63479657c1e2e204116b728fb16c89620e167
LewisPearsonGitHub <lewiscpearson@gmail.com>
2022-06-08 11:20:36 +0100

    Updated IMS file to include Orders

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -7,7 +7,9 @@ import com.qa.ims.controller.Action;
 import com.qa.ims.controller.CrudController;
 import com.qa.ims.controller.CustomerController;
 import com.qa.ims.controller.ItemsController;
+import com.qa.ims.controller.OrdersController;
 import com.qa.ims.persistence.dao.ItemsDAO;
+import com.qa.ims.persistence.dao.OrdersDAO;
 import com.qa.ims.persistence.dao.CustomerDAO;
 import com.qa.ims.persistence.domain.Domain;
 import com.qa.ims.utils.DBUtils;
@@ -20,11 +22,14 @@ public class IMS {
 	private final CustomerController customers;
 	private final ItemsController items;
 	private final Utils utils;
+	private final OrdersController orders;
 
 	public IMS() {
 		this.utils = new Utils();
 		final CustomerDAO custDAO = new CustomerDAO();
 		final ItemsDAO itDAO = new ItemsDAO();
+		final OrdersDAO orDAO = new OrdersDAO();
+		this.orders = new OrdersController(orDAO, utils);
 		this.items = new ItemsController(itDAO, utils);
 		this.customers = new CustomerController(custDAO, utils);
 	}
@@ -58,6 +63,7 @@ public class IMS {
 				active = this.items;
 				break;
 			case ORDER:
+				active = this.orders;
 				break;
 			case STOP:
 				return;

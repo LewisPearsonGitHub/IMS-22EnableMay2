6091c5d494a1c940d7656f2a6c1ae80a894ded00
LewisPearsonGitHub <lewiscpearson@gmail.com>
2022-06-09 12:50:49 +0100

    LPP-56 #done #comment Testing for items

--- src/main/java/com/qa/ims/persistence/domain/Items.java
+++ src/main/java/com/qa/ims/persistence/domain/Items.java
@@ -1,5 +1,7 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.Objects;
+
 public class Items {
 	
 	private Long itemsId;
@@ -50,23 +52,25 @@ public class Items {
 		return "id:" + itemsId + " value : £" + value + " item name:" + itemName;
 	}
 
-//	@Override
-//	public int hashCode() {
-//		return Objects.hash(itemName, itemsId, value);
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Items other = (Items) obj;
-//		return Objects.equals(itemName, other.itemName) && Objects.equals(itemsId, other.itemsId)
-//				&& Double.doubleToLongBits(value) == Double.doubleToLongBits(other.value);
-//	}	
+	@Override
+	public int hashCode() {
+		return Objects.hash(itemName, itemsId, value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Items other = (Items) obj;
+		return Objects.equals(itemName, other.itemName) && Objects.equals(itemsId, other.itemsId)
+				&& Double.doubleToLongBits(value) == Double.doubleToLongBits(other.value);
+	}
+
+	
 	
 	
 	
---
+++ src/test/java/com/qa/ims/persistence/dao/ItemsDAOTest.java
@@ -0,0 +1,58 @@
+package com.qa.ims.persistence.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.qa.ims.persistence.domain.Items;
+import com.qa.ims.utils.DBUtils;
+
+public class ItemsDAOTest {
+	private final ItemsDAO DAO = new ItemsDAO();
+
+	@Before
+	public void setup() {
+		DBUtils.connect();
+		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+	}
+
+	@Test
+	public void testCreate() {
+		final Items created = new Items(2L, 5, "dog");
+		assertEquals(created, DAO.create(created));
+	}
+
+	@Test
+	public void testReadAll() {
+		List<Items> expected = new ArrayList<>();
+		expected.add(new Items(1L, 1.20, "car"));
+		assertEquals(expected, DAO.readAll());
+	}
+
+	@Test
+	public void testReadLatest() {
+		assertEquals(new Items(1L, 1.20, "car"), DAO.readLatest());
+	}
+
+	@Test
+	public void testRead() {
+		final long itemsId = 1L;
+		assertEquals(new Items(itemsId, 1.20, "car"), DAO.read(itemsId));
+	}
+
+	@Test
+	public void testUpdate() {
+		final Items updated = new Items(1L, 5, "cat");
+		assertEquals(updated, DAO.update(updated));
+
+	}
+
+	@Test
+	public void testDelete() {
+		assertEquals(1, DAO.delete(1));
+	}
+}
